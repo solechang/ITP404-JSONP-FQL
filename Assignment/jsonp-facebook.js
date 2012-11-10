3ad7eb767e1484dc7d7a0b2927f68c217357888a
Jake Choi <changwoo92@gmail.com>
2012-11-09 16:10:01 -0800

    Finished JSONP assignment

---
+++ Assignment/jsonp-facebook.js
@@ -0,0 +1,23 @@
+var script = document.createElement('script');
+script.src = "https://graph.facebook.com/fql?q=SELECT+page_id,name,username,description,about,fan_count,pic_cover,pic_square,page_url+FROM+page+WHERE+username='blackhippymusic'+OR+username='KendrickLamarmusic'+OR+username='SchoolboyQ'+OR+username='AbSoulmusic'+OR+username='jayrock'+ORDER+BY+fan_count&callback=myFunction";
+document.getElementsByTagName('head')[0].appendChild(script);
+
+function myFunction(data) {
+	console.log(data);
+
+	//Handlebars is a global object when using handlebars
+	var templateString = document.getElementById('fb-page-template').innerHTML;
+	var template = Handlebars.compile(templateString);
+	var html = template(data);
+//	Handlebars.compile();
+/*
+	var p = "<p>" + data.name + "</p>";
+	var likes = "<span>" + data.likes + "</span>";
+
+	var html = p +  likes;
+	*/
+	document.getElementById('fb-page').innerHTML = html;
+
+
+
+}
